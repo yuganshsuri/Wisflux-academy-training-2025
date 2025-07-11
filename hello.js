39ffac60b6bc2e4e668a5a1879ce5067ce8ea295
yuganshsuri <suriyugansh2005@gmail.com>
2025-07-11 19:31:24 +0530

    Last commit of 11 july

--- hello.js
+++ hello.js
@@ -1,5 +1,45 @@
-console.log("Yuganshsuri")
-console.log("Oye lucky lucky oye ")
-naame="tonystark"
-console.log(naame)
-console.log(typeof(naame))
+alert("This might be my first js program")
+// It displays a pop up message in browser
+console.log("This is your first js program")
+//it prints in console
+// Varible declaration
+var name="Amit"
+let lastName="suri"
+const dateofBirth="8-10-2005"
+console.log(name);
+var name="Yugansh"
+console.log(name);
+lastName="sharma"
+console.log(lastName);
+console.log (typeof(name));
+console.log (typeof(lastName))
+console.log (typeof(dateofBirth))
+//this program will tell us about non primitive data type (objects)
+let pencil={name:"natraj",
+qty:3,
+color:"red",
+}
+console.log(pencil);
+console.log(pencil["color"]);
+//javascript is a dynamic language in which the data type is declared itself
+let a="siri"
+let b=true
+let c=null
+let d=undefined
+let e=78393
+let f=Symbol("Smile please")
+let g=BigInt("373737")
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log(typeof(g));
